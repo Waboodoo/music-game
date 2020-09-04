8207f1de9908c26bdac8d898c77a8b01d9beeb5d
Roland Meyer <roland.meyer@beekeeper.io>
2020-09-04 15:45:43 +0200

    Fixed loading of theory documents

--- src/assets/scripts/gameUI.js
+++ src/assets/scripts/gameUI.js
@@ -1,17 +1,17 @@
 const GameUI = {
-    
+
     _eventListener: null,
-    
+
     _state: {
         previousScore: 0,
         totalScore: 0,
     },
-    
+
     init(eventListener) {
         this._eventListener = eventListener;
         this._bindUIElements();
     },
-    
+
     _bindUIElements() {
         select('#musicToggle').mouseClicked(() => {
             this._triggerEvent(GameUIEvent.TOGGLE_MUSIC);
@@ -25,7 +25,7 @@ const GameUI = {
         select('#theoryButton').mouseClicked(() => {
             this._triggerEvent(GameUIEvent.THEORY);
         });
-        
+
         for (let i = 1; i <= 6; i++) {
             const index = i - 1;
             select(`#option${i}`).mouseClicked(() => {
@@ -35,17 +35,17 @@ const GameUI = {
             });
         }
     },
-    
+
     _triggerEvent(eventName, data) {
         this._eventListener(eventName, data);
     },
-    
+
     resetCanvas(colorStyle) {
         clear();
         background(this._getBackgroundImage(colorStyle));
         select('#canvasContainer').class(`colorStyle${colorStyle}`);
     },
-    
+
     _getBackgroundImage(colorStyle) {
         return Assets.images[`background${colorStyle}`];
     },
@@ -59,7 +59,7 @@ const GameUI = {
             select('#nextLevel').removeClass('visible');
         }
     },
-    
+
     setPauseButtonVisible(isVisible) {
         if (isVisible) {
             select('#gameToggle').addClass('visible');
@@ -67,21 +67,21 @@ const GameUI = {
             select('#gameToggle').removeClass('visible');
         }
     },
-    
+
     drawLevelNumber(levelNumber) {
         this._drawMainText(`Level ${levelNumber}`);
     },
-    
+
     drawCompleteState() {
         this._drawMainText('Game Over');
     },
-    
+
     setOptions(options) {
         for (let i = 1; i <= 6; i++) {
             select(`#optionText${i}`).html(options[i-1]);
         }
     },
-    
+
     setOptionsVisibility(isVisible) {
         for (let i = 1; i <= 6; i++) {
             const index = i - 1;
@@ -92,21 +92,21 @@ const GameUI = {
             }
         }
     },
-    
+
     _drawMainText(message) {
         fill(0, 0, 0);
         textSize(36);
         textAlign(CENTER);
         text(message, width / 2, 60);
     },
-    
+
     setTotalScore(totalScore) {
         if (totalScore != this._state.totalScore){
             this._state.previousScore = this._state.totalScore;
             this._state.totalScore = totalScore;
         }
     },
-    
+
     drawScore() {
         fill(255, 255, 255);
         textSize(30);
@@ -120,12 +120,12 @@ const GameUI = {
         stroke("#000000");
         strokeWeight(1);
     },
-    
+
     setLevelScore(levelScore, levelCompletionScore) {
         this._state.levelScore = levelScore;
         this._state.levelCompletionScore = levelCompletionScore;
     },
-    
+
     drawLevelScore() {
         const levelScore = this._state.levelScore;
         const levelCompletionScore = this._state.levelCompletionScore;
@@ -134,7 +134,7 @@ const GameUI = {
         textAlign(CENTER);
         text(`${levelScore} / ${levelCompletionScore}`, width / 2, 160);
     },
-    
+
     setMusicState(isPlaying) {
         if (isPlaying) {
             select(`#musicToggle`).addClass('playing');
@@ -142,7 +142,7 @@ const GameUI = {
             select(`#musicToggle`).removeClass('playing');
         }
     },
-    
+
     setGameButtonState(isRunning) {
         if (isRunning) {
             select(`#gameToggle`).removeClass('paused');
@@ -150,17 +150,22 @@ const GameUI = {
             select(`#gameToggle`).addClass('paused');
         }
     },
-    
+
     showTheory(uri) {
         const frame = select(`#theoryFrame`);
+        const pdfContainer = createElement('object');
+        pdfContainer.attribute('data', uri);
+        frame.html('');
+        frame.child(pdfContainer);
         frame.addClass('visible');
-        frame.attribute('data', uri);
         select('.text', `#theoryButton`).html('OK');
     },
-    
+
     hideTheory() {
-        select(`#theoryFrame`).removeClass('visible');
+        const frame = select(`#theoryFrame`);
+        frame.html('');
+        frame.removeClass('visible');
         select('.text', `#theoryButton`).html('Theorie');
     },
-    
+
 };
